a297f9ba0d0fda75bf3cda598693f8162ec5f092
peterjosephgreen-cell <peterjosephgreen@hotmail.com>
2026-08-24 11:45:39 +1000

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.CLUB_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAb4t3-BRSN_pL6H-uFKK1AwXb2VQA0qo4",
+  authDomain: "central-coast-board-games.firebaseapp.com",
+  projectId: "central-coast-board-games",
+  storageBucket: "central-coast-board-games.firebasestorage.app",
+  messagingSenderId: "588281431396",
+  appId: "1:588281431396:web:a24bf8347d426a6118e671"
+};
