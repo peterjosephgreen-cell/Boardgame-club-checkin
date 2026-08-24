a72aeccf8c27cb91e411d37eacc63f6efe609808
peterjosephgreen-cell <peterjosephgreen@hotmail.com>
2026-08-24 10:35:39 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.CLUB_CONFIG = {
-  SUPABASE_URL: "https://wpstltltcevcnokmajsw.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_SZrWCisMZ7gP-kqsSNI8_g_juYAkWWo"
+  SUPABASE_URL: https://wpstltltcevcnokmajsw.supabase.co,
+  SUPABASE_ANON_KEY: sb_publishable_SZrWCisMZ7gP-kqsSNI8_g_juYAkWWo
 };
